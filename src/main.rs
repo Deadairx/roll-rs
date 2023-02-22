96bfb39121518668fdd09ac70bfc3a938f76dfb9
DeadAirX <djdivinity7@gmail.com>
2023-02-21 22:13:56 -0600

    refactor: return RollResult

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
 use regex::Regex;
 use rand::{thread_rng, Rng};
 use std::env;
+use std::fmt::Display;
 
-fn roll_dice(num_dice: u32, num_sides: u32, drop_action: char, modifier: i32) -> i32 {
+enum DropAction {
+    DropHighest,
+    DropLowest,
+    Keep,
+}
+
+impl Display for DropAction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DropAction::DropHighest => write!(f, "DropHighest"),
+            DropAction::DropLowest => write!(f, "DropLowest"),
+            DropAction::Keep => write!(f, "Keep"),
+        }
+    }
+}
+
+struct RollResult {
+    num_dice: u32,
+    sides: u32,
+    drop_action: DropAction,
+    individual_rolls: Vec<u32>,
+    modifier: i32,
+    result_total: i32,
+}
+
+fn roll_dice(num_dice: u32, num_sides: u32, drop_action: char, modifier: i32) -> RollResult {
     let mut rng = thread_rng();
     let mut dice = Vec::new();
     for _ in 0..num_dice {
         dice.push(rng.gen_range(1..num_sides + 1) as i32);
     }
-    println!("drop: {}", drop_action);
-    println!("rolls: {:?}", dice);
     if drop_action == 'L' {
         dice.sort();
         dice.reverse();
@@ -18,9 +42,21 @@ fn roll_dice(num_dice: u32, num_sides: u32, drop_action: char, modifier: i32) ->
         dice.sort();
         dice.pop();
     }
-    println!("dice count: {}", dice.len());
     let result: i32 = dice.iter().sum();
-    result + modifier
+    let result_total = result + modifier;
+
+    RollResult {
+        num_dice,
+        sides: num_sides,
+        drop_action: match drop_action {
+            'L' => DropAction::DropLowest,
+            'H' => DropAction::DropHighest,
+            _ => DropAction::Keep,
+        },
+        individual_rolls: dice.iter().map(|x| *x as u32).collect(),
+        modifier,
+        result_total,
+    }
 }
 
 /// Takes a Dice Notation string and parses out the number of dice, number of sides and
@@ -44,5 +80,8 @@ fn main() {
     let notation = &args[1];
     let (num_dice, num_sides, drop_action, modifier) = parse_dice_notation(notation);
     let result = roll_dice(num_dice, num_sides, drop_action, modifier);
-    println!("Result: {}", result);
+    println!("drop: {}", result.drop_action);
+    println!("rolls: {:?}", result.individual_rolls);
+    println!("dice count: {}", result.num_dice);
+    println!("Result: {}", result.result_total);
 }
