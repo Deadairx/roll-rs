d6692c4a5f8506bf493da1bf1cf030eb29a00ac9
DeadAirX <djdivinity7@gmail.com>
2023-02-28 10:44:17 -0600

    Add lib for 3rd party use AND include dropped dice in roll

--- src/roller.rs
+++ src/roller.rs
@@ -26,31 +26,54 @@ pub struct RollResult {
     pub result_total: i32,
 }
 
+impl Default for RollResult {
+    fn default() -> Self {
+        Self {
+            num_dice: 0,
+            sides: 0,
+            drop_action: DropAction::Keep,
+            individual_rolls: Vec::new(),
+            modifier: 0,
+            result_total: 0,
+        }
+    }
+}
+
 pub fn roll_dice(num_dice: u32, num_sides: u32, drop_action: char, modifier: i32) -> RollResult {
     let mut rng = thread_rng();
     let mut dice = Vec::new();
     for _ in 0..num_dice {
         dice.push(rng.gen_range(1..num_sides + 1) as i32);
     }
-    if drop_action == 'L' {
-        dice.sort();
-        dice.reverse();
-        dice.pop();
-    } else if drop_action == 'H' {
-        dice.sort();
-        dice.pop();
-    }
-    let result: i32 = dice.iter().sum();
+
+    let drop_action = match drop_action {
+        'L' => DropAction::DropLowest,
+        'H' => DropAction::DropHighest,
+        _ => DropAction::Keep,
+    };
+
+    //if drop_action == 'L' {
+        //dice.sort();
+        //dice.reverse();
+        //dice.pop();
+    //} else if drop_action == 'H' {
+        //dice.sort();
+        //dice.pop();
+    //}
+
+    let drop_amount = match drop_action {
+        DropAction::DropLowest => dice.iter().min().unwrap_or(&0),
+        DropAction::DropHighest => dice.iter().max().unwrap_or(&0),
+        DropAction::Keep => &0
+    };
+
+    let result: i32 = (dice.iter().sum::<i32>()) - *drop_amount;
     let result_total = result + modifier;
 
     RollResult {
         num_dice,
         sides: num_sides,
-        drop_action: match drop_action {
-            'L' => DropAction::DropLowest,
-            'H' => DropAction::DropHighest,
-            _ => DropAction::Keep,
-        },
+        drop_action,
         individual_rolls: dice.iter().map(|x| *x as u32).collect(),
         modifier,
         result_total,
